8d61d83e858bd23d20a8d9c803d25b792f635883
Николай Васильев <nikolay.vasilyev@uksever-invest.ru>
2022-12-28 00:20:43 +0300

    fix script

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,4 +5,4 @@ set -euxo pipefail
 python manage.py collectstatic --no-input
 #bash ./wait.sh "$POSTGRES_HOST:$POSTGRES_PORT"
 python manage.py migrate --no-input
-gunicorn severinvest.wsgi --bind 0.0.0.0:8000 --workers 4 --timeout 60
+gunicorn notes_backend.wsgi --bind 0.0.0.0:8000 --workers 4 --timeout 60
